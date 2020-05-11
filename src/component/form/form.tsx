59cedf3c85bc1f625d622c8387a36e3efe60340c
mansi1 <m.mansi1@gmx.net>
2020-05-11 21:06:59 +0200

    initial commit

---
+++ src/component/form/form.tsx
@@ -0,0 +1,176 @@
+import {attr, component, event} from "springtype/web/component";
+import {st} from "springtype/core";
+import {tsx} from "springtype/web/vdom";
+import {IEventListener, ILifecycle} from "springtype/web/component/interface";
+import {ref} from "springtype/core/ref";
+import {htmlCollectionToArray, TYPE_FUNCTION} from "springtype/core/lang";
+import {Validation} from "..";
+import {getFormConfig} from "../../config";
+
+export interface FromValidationDetail {
+    valid: boolean,
+    state: any
+}
+
+export interface IAttrForm {
+    name?: string;
+}
+
+export const FORM_PROPERTY_NAME = "FROM";
+export const FORM_IGNORE_PROPERTY_NAME = "FORM_IGNORE";
+export const FORM_VALUE_FUNCTION_KEY = "FORM_VALUE_FUNCTION";
+
+@component
+export class Form extends st.component<IAttrForm> {
+
+    @attr
+    name: string = "form";
+
+    @ref
+    formRef!: HTMLFormElement;
+
+    @event
+    onFormValidation!: IEventListener<Event>;
+
+    validationReject!: (reason?: any) => void;
+
+    dispatchFormValidation = (detail: FromValidationDetail) => {
+        this.dispatchEvent<FromValidationDetail>("formValidation", {
+            bubbles: true,
+            cancelable: true,
+            composed: true,
+            detail: {
+                ...detail,
+            },
+        });
+    };
+
+
+    render() {
+        return <form ref={{formRef: this}}>
+                {this.renderChildren()}
+            </form>
+
+    }
+
+    onAfterRender(): void {
+        this.addFormToForm();
+        this.overrideSubmit();
+    }
+
+    addFormToForm() {
+        (this.formRef as any)[FORM_PROPERTY_NAME] = this;
+    }
+
+    overrideSubmit() {
+        //ignore on submit validate forms async
+        this.formRef.addEventListener('submit', (evt) => {
+            evt.preventDefault();
+
+        })
+    }
+
+    async validate(force: boolean = false): Promise<boolean> {
+        return await new Promise(async (resolve) => {
+            let result = true;
+            const elementPromises = await Promise.all(this.getElements().map(element => element.validate(force)));
+            if (elementPromises.filter(v => !v).length > 0) {
+                this.formRef.checkValidity();
+                result = false;
+            }
+            const subFormPromises = await Promise.all(this.getSubForm().map(element => element.validate(force)));
+            if (subFormPromises.filter(v => !v).length > 0) {
+                result = false;
+            }
+            const validateResult: FromValidationDetail = {
+                valid: result,
+                state: this.getState()
+            };
+            this.dispatchFormValidation(validateResult);
+            resolve(result);
+        });
+    }
+
+    getElements(): Array<Validation> {
+        //get validator name form configuration
+        const validatorPropertyName = getFormConfig().validationPropertyName;
+
+        const validationComponents: Array<Validation> = [];
+        for (const element of htmlCollectionToArray<any>((this.formRef.elements))) {
+            if (element[validatorPropertyName] && element[validatorPropertyName] instanceof Validation) {
+                const validationComponent = element[validatorPropertyName] as Validation;
+                if (!element.disabled && !element.readonly) {
+                    validationComponents.push(validationComponent);
+                }
+            }
+        }
+        return validationComponents;
+    }
+
+    getState(): any {
+        const formState: { [key: string]: any } = {};
+        const radios: { [name: string]: RadioNodeList } = {};
+        const elements = this.formRef.elements;
+        for (const element of htmlCollectionToArray<HTMLElement>(elements)) {
+            const anyElement = element as any;
+            if (
+                element instanceof HTMLButtonElement
+                || anyElement[FORM_IGNORE_PROPERTY_NAME]) {
+                continue
+            }
+            if (anyElement[FORM_VALUE_FUNCTION_KEY] && typeof anyElement[FORM_VALUE_FUNCTION_KEY] === TYPE_FUNCTION) {
+                formState[anyElement.name] = anyElement[FORM_VALUE_FUNCTION_KEY]();
+                continue;
+            }
+
+            if (element instanceof HTMLInputElement) {
+                const htmlInput = element as HTMLInputElement;
+                if (htmlInput.type === 'radio' && htmlInput.name) {
+                    radios[htmlInput.name] = elements.namedItem(htmlInput.name) as RadioNodeList;
+                    continue;
+                }
+                if (htmlInput.type === 'checkbox' && htmlInput.name) {
+                    formState[htmlInput.name] = htmlInput.checked;
+                    continue;
+                }
+            }
+            const htmlElement = (element as any);
+            const elementName = htmlElement.name;
+            formState[elementName] = htmlElement.value;
+        }
+        for (const radioGroupName of Object.keys(radios)) {
+            formState[radioGroupName] = radios[radioGroupName].value;
+        }
+        for (const form of this.getSubForm()) {
+            formState[form.name] = form.getState();
+        }
+        return formState;
+    }
+
+    getSubForm(): Array<Form> {
+        const forms: Array<Form> = [];
+        for (const form of htmlCollectionToArray<any>(this.formRef.querySelectorAll('form'))) {
+            if (form[FORM_PROPERTY_NAME] && form[FORM_PROPERTY_NAME] instanceof Form) {
+                const nestedForm = form[FORM_PROPERTY_NAME] as Form;
+                if (!!nestedForm && nestedForm === this) {
+                    forms.push(nestedForm);
+                    continue;
+                }
+                let parent: ILifecycle | undefined = nestedForm.parent;
+                while (!!parent) {
+                    parent = parent.parent;
+                    if (!!parent && parent instanceof Form && parent === this) {
+                        forms.push(nestedForm);
+                    }
+                }
+            } else {
+                st.error('Using an nested form, please use <Form name="formName">', form);
+            }
+        }
+        return forms;
+    }
+
+    reset() {
+        this.formRef.reset();
+    }
+}
---
+++ src/config.ts
@@ -0,0 +1,54 @@
+import {globalThis} from "springtype/core";
+
+export const WINDOW_GLOBAL_FORM_CONFIG_KEY = '$stForm';
+
+export const DEFAULT_ST_FORM_LOG_DEBUG_MESSAGE_KEY = 'ST_FORM_LOG_DEBUG_MESSAGE'
+export const DEFAULT_ST_FORM_VALIDATION_PROPERTY_NAME_KEY = 'ST_FORM_VALIDATION_PROPERTY_NAME'
+export const DEFAULT_ST_FORM_VALIDATION_DEBOUNCE_TIME_IN_MS_KEY = 'ST_FORM_VALIDATION_DEBOUNCE_TIME_IN_MS'
+export const DEFAULT_ST_FORM_VALIDATION_EVENT_LISTENER_KEY = 'ST_FORM_VALIDATION_EVENT_LISTENER'
+
+export interface FormConfig {
+    validationDebounceTimeInMs: number;
+    validationEventListener: Array<string>;
+    validationPropertyName: string;
+    logDebugMessages: boolean;
+}
+
+const getValidationEventListenerFromEnv =() => {
+    const env = process.env[DEFAULT_ST_FORM_VALIDATION_EVENT_LISTENER_KEY];
+    if(env){
+        return env.split('|');
+    }
+}
+
+const getLogDebugFromEnv = () => {
+    const env = process.env[DEFAULT_ST_FORM_LOG_DEBUG_MESSAGE_KEY];
+    if(env){
+        return env === 'true';
+    }
+}
+
+const getValidationDebunceTimeInMsFromEnv = () => {
+    const env = process.env[DEFAULT_ST_FORM_VALIDATION_DEBOUNCE_TIME_IN_MS_KEY];
+    if(env){
+        return parseInt(env);
+    }
+}
+export const defaultFormConfig: () => FormConfig = () => {
+    return {
+        logDebugMessages: getLogDebugFromEnv() || false,
+        validationPropertyName: process.env[DEFAULT_ST_FORM_VALIDATION_PROPERTY_NAME_KEY] || 'VALIDATION',
+        validationDebounceTimeInMs: getValidationDebunceTimeInMsFromEnv() || 250,
+        validationEventListener: getValidationEventListenerFromEnv() || ['change', 'keyup']
+    }
+};
+
+globalThis[WINDOW_GLOBAL_FORM_CONFIG_KEY] = defaultFormConfig();
+
+export const getFormConfig = (): FormConfig => {
+    return globalThis[WINDOW_GLOBAL_FORM_CONFIG_KEY]
+};
+
+
+
+
